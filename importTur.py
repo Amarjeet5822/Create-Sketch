0bc20a8559b18e8f55b146a4a9356a851de94777
Amarjeet5822 <amar.bst5822@gmail.com>
2024-02-04 10:18:42 +0530

    I make star, along circle with that

--- importTur.py
+++ importTur.py
@@ -1,6 +1,14 @@
- 
+# by using turtle Library drawing an area
 import turtle
 myStar = turtle.Turtle()
-
-myStar.right(125)
-myStar.forward(150)
+turtle.speed(1000)
+myStar.right(75)
+myStar.forward(100)
+for i in range(5):
+    myStar.right(144)
+    myStar.circle(30)
+    myStar.color("red")
+    myStar.fillcolor("red")    
+    myStar.forward(100)
+    myStar.circle(30)
+turtle.done()
