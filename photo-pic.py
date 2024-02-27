3797a76494d214a4bd219e05d8c3d8daaa03d0d2
Amarjeet5822 <amar.bst5822@gmail.com>
2024-02-27 20:06:47 +0530

    I make Picture by using Library and Canva

---
+++ photo-pic.py
@@ -0,0 +1,10 @@
+from sketchpy import canvas
+from sketchpy import library
+
+# 1st Make picture of A.P.J Abdul Kalam Sir "Missile Man of India"
+
+# obj = library.apj()
+
+# 2nd Make picture of Lord Krishna
+obj = canvas.sketch_from_image(r"C:\\Users\\AMARJEET\\Downloads\\Lord Krishna.jpg")
+obj.draw()
